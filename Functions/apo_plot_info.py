9563a8a209071c713ee6989cad2306f569fc0782
vf20487 <hannah.chawner@bristol.ac.uk>
2023-02-09 16:20:43 +0000

    add no ocean colour

--- Functions/apo_plot_info.py
+++ Functions/apo_plot_info.py
@@ -1,5 +1,5 @@
 # colours to use for each source/sim - taken from colorblind friendly palette below
-colors = {'ecco': '#377eb8', 'jena': '#4daf4a', 'nemo': '#984ea3', 'ff': '#ff7f00', 'obs': '#f781bf'}
+colors = {'ecco': '#377eb8', 'jena': '#4daf4a', 'nemo': '#984ea3', 'ff': '#ff7f00', 'obs': '#f781bf', 'no ocean': 'grey'}
 colors_sources = {f'ocean_{sim.lower()}' if sim.lower() in ['ecco', 'jena', 'nemo'] else sim.lower(): col
                   for sim, col in colors.items()}
 
@@ -7,13 +7,21 @@ colors_sources = {f'ocean_{sim.lower()}' if sim.lower() in ['ecco', 'jena', 'nem
 ls = {'o2': '-', 'co2': '--', 'n2': '-.', 'obs': 'none'}
 
 # names of the models
-model_names = {'ecco': 'ECCO', 'jena': 'Jena', 'nemo': 'NEMO-ERSEM'}
+model_names = {'ecco': 'ED', 'jena': 'JC', 'nemo': 'NE', 'no ocean': 'no ocean'}
+baseline_names = {'jena': 'JC', 'rebs': 'REBS'}
 
 # adjustment to APO baseline
-adjust = {2014: {1:0, 2:0, 3:15, 4:0, 5:0, 6:10, 7:5, 8:10, 9:0, 10:0, 11:10, 12:10},
-          2015: {1:10, 2:0, 3:15, 4:10, 5:10, 6:10, 7:5, 8:10, 9:0, 10:0, 11:10, 12:5},
-          2018: {1:0, 2:15, 3:40, 4:50, 5:50, 6:20, 7:0, 8:0, 9:15, 10:20, 11:20, 12:15},
-          2020: {1:15, 2:30, 3:25, 4:40, 5:25, 6:0, 7:-5, 8:-10, 9:-5, 10:0, 11:0, 12:30}}
+adjust = {2014: {'WAO': {1:0, 2:0, 3:15, 4:0, 5:0, 6:10, 7:5, 8:10, 9:0, 10:0, 11:10, 12:10}},
+          2015: {'WAO': {1:10, 2:0, 3:15, 4:10, 5:10, 6:10, 7:5, 8:10, 9:0, 10:0, 11:10, 12:5}},
+          2018: {'WAO': {1:0, 2:15, 3:40, 4:50, 5:50, 6:20, 7:0, 8:0, 9:15, 10:20, 11:20, 12:15}},
+          2020: {'WAO': {1:15, 2:30, 3:25, 4:40, 5:25, 6:0, 7:-5, 8:-10, 9:-5, 10:0, 11:0, 12:30}},
+          2021: {'WAO': {1:0, 2:10, 3:18, 4:20, 5:-10, 6:-25, 7:-40, 8:-30, 9:-30, 10:-30, 11:-20, 12:0},
+                 'HFD': {1:0, 2:10, 3:18, 4:20, 5:-10, 6:-25, 7:-30, 8:-30, 9:-15, 10:-10, 11:0, 12:10}}}
+
+for yr in adjust.keys():
+       for site in ['HFD', 'RGL']:
+              adjust[yr][site ] = adjust[yr][site ] if site in adjust[yr].keys() else\
+                                  {mm: 0 for mm in range(1, 13)}
 
 # colorblind friendly palette: https://gist.github.com/thriveth/8560036
 # blue, orange, green,
